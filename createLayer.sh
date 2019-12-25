c2aed00538b1cbbe9896980cb1a568c273d42d39
tobilg <tobilg@gmail.com>
2019-12-25 10:39:37 +0100

    Update Tile38 version to 1.19.3

--- createLayer.sh
+++ createLayer.sh
@@ -5,14 +5,15 @@ mkdir -p layer/tile38
 cd layer
 
 # Download Tile38 release
-curl -L  https://github.com/tidwall/tile38/releases/download/1.17.2/tile38-1.17.2-linux-amd64.tar.gz -o tile38-1.17.2-linux-amd64.tar.gz
+curl -L https://github.com/tidwall/tile38/releases/download/1.19.3/tile38-1.19.3-linux-amd64.tar.gz -o tile38-1.19.3-linux-amd64.tar.gz
 
 # Unzip
-tar xzvf tile38-1.17.2-linux-amd64.tar.gz
+tar xzvf tile38-1.19.3-linux-amd64.tar.gz
 
-# Move server binary
-mv tile38-1.17.2-linux-amd64/tile38-server tile38/tile38-server
+# Move server and cli binary
+mv tile38-1.19.3-linux-amd64/tile38-server tile38/tile38-server
+mv tile38-1.19.3-linux-amd64/tile38-cli tile38/tile38-cli
 
 # Cleanup
-rm -rf tile38-1.17.2-linux-amd64/
-rm tile38-1.17.2-linux-amd64.tar.gz
+rm -rf tile38-1.19.3-linux-amd64/
+rm tile38-1.19.3-linux-amd64.tar.gz
