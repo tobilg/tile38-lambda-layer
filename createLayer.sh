0a512aed3729f0526ec50fa56aaccd155c6dd120
tobilg <tobilg@gmail.com>
2020-08-01 18:30:24 +0200

    Version bump to 1.21.1

--- createLayer.sh
+++ createLayer.sh
@@ -5,15 +5,15 @@ mkdir -p layer/tile38
 cd layer
 
 # Download Tile38 release
-curl -L https://github.com/tidwall/tile38/releases/download/1.19.3/tile38-1.19.3-linux-amd64.tar.gz -o tile38-1.19.3-linux-amd64.tar.gz
+curl -L https://github.com/tidwall/tile38/releases/download/1.21.1/tile38-1.21.1-linux-amd64.tar.gz -o tile38-1.21.1-linux-amd64.tar.gz
 
 # Unzip
-tar xzvf tile38-1.19.3-linux-amd64.tar.gz
+tar xzvf tile38-1.21.1-linux-amd64.tar.gz
 
 # Move server and cli binary
-mv tile38-1.19.3-linux-amd64/tile38-server tile38/tile38-server
-mv tile38-1.19.3-linux-amd64/tile38-cli tile38/tile38-cli
+mv tile38-1.21.1-linux-amd64/tile38-server tile38/tile38-server
+mv tile38-1.21.1-linux-amd64/tile38-cli tile38/tile38-cli
 
 # Cleanup
-rm -rf tile38-1.19.3-linux-amd64/
-rm tile38-1.19.3-linux-amd64.tar.gz
+rm -rf tile38-1.21.1-linux-amd64/
+rm tile38-1.21.1-linux-amd64.tar.gz
